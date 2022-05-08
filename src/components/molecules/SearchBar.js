1393d7831a2e368fd1468b7623dd73d0360f850f
dinadeljanin <32048693+dinadeljanin@users.noreply.github.com>
2022-05-08 17:18:46 -0400

    Added search SVG

--- src/components/molecules/SearchBar.js
+++ src/components/molecules/SearchBar.js
@@ -2,17 +2,29 @@ import React from 'react'
 
 import styled from 'styled-components'
 
+import search from '../../assets/search.svg'
+
 const Wrap = styled.div`
-	${'' /* Hardcoding until input */}
 	height: 2em;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	padding: 0 0 0 0.5em;
+	gap: 0.25em;
 	background-color: #F2F4F6;
 	box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
 	border-radius: 4px;
+	img {
+		height: 1em;
+	}
 `
 
 const SearchBar = () => {
 	return (
-		<Wrap>Search</Wrap>
+		<Wrap>
+			<img src={search} alt=""/>
+			Search
+		</Wrap>
 	)
 }
 
