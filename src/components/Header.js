91a0786653fa6e70277fb15639d251fffca45840
Brent Conn <brent.conn@consensys.net>
2022-05-08 17:04:56 -0400

    anchor

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import React from 'react'
 import styled from 'styled-components'
 
 import mmbrand from '../assets/mm_brand.svg'
+import Anchor from "./ui/atoms/Anchor";
 
 const Header = () => {
 	return (
 		<header>
 			<img src={mmbrand} alt="MetaMask"/>
+			<Anchor link="https://google.com" name="gooooogle">Google</Anchor>
 		</header>
 	)
 }
