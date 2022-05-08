5855a16aafea139b29ddcfd160dcf219735f1fbf
dinadeljanin <32048693+dinadeljanin@users.noreply.github.com>
2022-05-08 17:23:19 -0400

    Added input component to searchbar.

--- src/components/molecules/SearchBar.js
+++ src/components/molecules/SearchBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import styled from 'styled-components'
 
+import { UnlabeledInput as Input } from '../atoms/Input'
 import search from '../../assets/search.svg'
 
 const Wrap = styled.div`
@@ -9,7 +10,7 @@ const Wrap = styled.div`
 	display: flex;
 	flex-direction: row;
 	align-items: center;
-	padding: 0 0 0 0.5em;
+	padding: 0 0.35em 0 0.5em;
 	gap: 0.25em;
 	background-color: #F2F4F6;
 	box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
@@ -17,13 +18,19 @@ const Wrap = styled.div`
 	img {
 		height: 1em;
 	}
+	input {
+		border: none;
+		outline: none;
+		background-color: transparent;
+		width: 100%;
+	}
 `
 
 const SearchBar = () => {
 	return (
 		<Wrap>
 			<img src={search} alt=""/>
-			Search
+			<Input />
 		</Wrap>
 	)
 }
