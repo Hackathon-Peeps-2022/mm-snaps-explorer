7120c993cdfe70fc1243efe85f4173ba3e90fa86
Brent Conn <brent.conn@consensys.net>
2022-05-10 11:04:53 -0400

    router

---
+++ src/pages/AnotherPage.js
@@ -0,0 +1,11 @@
+import GlobalStyles from '../theme/GlobalStyles'
+
+const AnotherPage = () => {
+  return (
+		<>
+			<img src="https://www.clipartmax.com/png/full/202-2026488_tails-gets-trolled-face.png"/>
+		</>
+  );
+}
+
+export default AnotherPage;
