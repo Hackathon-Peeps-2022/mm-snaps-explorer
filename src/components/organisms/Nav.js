e312e930fc317457a785a78cbd5ae9f753ad9c51
jenninguyen <jenni.nguyen@consensys.net>
2022-05-11 17:22:57 -0400

    added icons to nav

--- src/components/organisms/Nav.js
+++ src/components/organisms/Nav.js
@@ -4,17 +4,24 @@ import SearchBar from "../molecules/SearchBar";
 
 import MenuLink from "../atoms/MenuLink";
 
-import star from "../../assets/star.svg";
+import categories from "../../assets/categories.svg";
+import nft from "../../assets/nft.svg";
+import iconStars from "../../assets/icon_stars.svg";
+import box from "../../assets/box.svg";
+import protocols from "../../assets/protocols.svg";
 
 const Nav = () => {
   return (
     <nav>
       <SearchBar />
-      <MenuLink link="/" name="Categories" icon={star}></MenuLink>
+      <MenuLink link="/" name="Categories" icon={categories}></MenuLink>
       <div style={{ position: "relative", left: "20px" }}>
-        <MenuLink link="/:nfts" name="NFTs" icon={star}></MenuLink>
+        <MenuLink link="/:nfts" name="NFTs" icon={nft}></MenuLink>
+        <MenuLink link="/:best" name="Best Rated" icon={iconStars}></MenuLink>
+        <MenuLink link="/:best" name="Utilities" icon={box}></MenuLink>
+        <MenuLink link="/:best" name="Protocols" icon={protocols}></MenuLink>
       </div>
-      <MenuLink link="test" name="Test" icon={star}></MenuLink>
+      <MenuLink link="test" name="Test" icon={categories}></MenuLink>
     </nav>
   );
 };
