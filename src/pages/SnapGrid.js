247b1948d8b04edbd5699fa5cec89cbb054d8b7a
Brent Conn <brent.conn@consensys.net>
2022-05-11 20:36:12 -0400

    I do a bit of trolling

--- src/pages/SnapGrid.js
+++ src/pages/SnapGrid.js
@@ -14,21 +14,20 @@ const SnapGrid = () => {
 
   return (
     <>
-      <h2>Snaps</h2>
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+          gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
           gap: "12px 24px",
         }}
       >
         {snaps.map((snap) => {
           let name = `${snap.creator} ${snap.installation} ${snap.title}`;
           return (
-            <div key={snap.id} style={{ position: "relative" }}>
+            <div key={snap.id} style={{ position: "relative", border: "1px dashed red" }}>
               <img
-                width={200}
-                height={200}
+                width={100}
+                height={100}
                 src={snap.iconUrl}
                 alt={name}
                 style={{
