bd089a230abf51b20e181e2c0817f3e5c84a3d4b
dinadeljanin.eth <32048693+dinadeljanin@users.noreply.github.com>
2022-05-12 09:54:52 -0400

    Vote (#12)
    * Added feature component // media queries pending.
    
    * Created SnapCards // WIP SnapItem and Recommended Section.
    
    * Made tweaks to featured snap items to switch bg color. Created install button component.
    
    * Forced styles to sit pretty.
    
    * disable stylelint
    
    * I do a bit of trolling
    
    * Redo git commit
    
    * Added vote component.
    
    * Vote count // new branch who dis
    
    * sitting pretty
    
    Co-authored-by: Brent Conn <brent.conn@consensys.net>

--- src/components/atoms/InstallButton.js
+++ src/components/atoms/InstallButton.js
@@ -6,7 +6,11 @@ const Container = styled.button`
   border: 2px solid #9c5add;
   background-color: transparent;
   padding: 0.25em 0.5em;
+  cursor: pointer;
   border-radius: 5px;
+  width: 100px;
+  height: 2.5em;
+  grid-area: install;
   font-family: "Euclid", serif;
 `;
 
