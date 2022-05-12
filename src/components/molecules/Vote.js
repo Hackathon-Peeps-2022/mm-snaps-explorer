91971aed075f78398753d9f0ea515d8466ed55f2
dinadeljanin <32048693+dinadeljanin@users.noreply.github.com>
2022-05-11 23:26:32 -0400

    Added vote component.

--- src/components/molecules/Vote.js
+++ src/components/molecules/Vote.js
@@ -1,41 +1,41 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import chevronUp from "../../assets/chevron-up.svg";
-import chevronDown from "../../assets/chevron-down.svg";
+import chevronUp from '../../assets/chevron-up.svg'
+import chevronDown from '../../assets/chevron-down.svg'
 
 const Wrap = styled.div`
-  grid-area: vote;
-  display: flex;
-  flex-direction: column;
-  width: 2em;
-  ${"" /* Smol hangup here is if we have more than single/double digit votes */}
-  align-items: center;
-  button {
-    height: 1em;
-    width: 1em;
-    background-position: center;
-    border: none;
-    cursor: pointer;
-  }
-  > button:first-of-type {
-    background-image: url(${chevronUp});
-  }
-  > button:last-of-type {
-    background-image: url(${chevronDown});
-  }
-`;
+	grid-area: vote;
+	display: flex;
+	flex-direction: column;
+	width: 2em;
+	${'' /* Smol hangup here is if we have more than single/double digit votes */}
+	align-items: center;
+	button {
+		height: 1em;
+		width: 1em;
+		background-position: center;
+		border: none;
+		cursor: pointer;
+	}
+	> button:first-of-type {
+		background-image: url(${chevronUp});
+	}
+	> button:last-of-type {
+		background-image: url(${chevronDown});
+	}
+`
 
 const Vote = () => {
-  const [vote, setVote] = useState(0);
-  return (
-    <Wrap>
-      <button onClick={() => setVote(vote + 1)} />
-      <p>{vote}</p>
-      <button onClick={() => setVote(vote - 1)} />
-    </Wrap>
-  );
-};
+	const [vote, setVote] = useState(0)
+	return (
+		<Wrap>
+			<button onClick={() => setVote(vote + 1)}/>
+			<p>{vote}</p>
+			<button onClick={() => setVote(vote - 1)}/>
+		</Wrap>
+	)
+}
 
-export default Vote;
+export default Vote
