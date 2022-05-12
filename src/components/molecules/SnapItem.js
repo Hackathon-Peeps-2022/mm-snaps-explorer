ef4c4208be2d0015fdde52667164e051c3de749c
dinadeljanin <32048693+dinadeljanin@users.noreply.github.com>
2022-05-11 23:23:51 -0400

    Redo git commit

--- src/components/molecules/SnapItem.js
+++ src/components/molecules/SnapItem.js
@@ -1,16 +1,44 @@
 import React from "react";
 
 import styled from "styled-components";
+import { transparentize } from "polished";
 
 import InstallButton from "../atoms/InstallButton";
+import Vote from "../molecules/Vote";
+
+import fil from "../../assets/icons/fil.svg";
 
 const Wrap = styled.div`
   border: 1px solid red;
   background-color: #f2f4f6;
   border-radius: 5px;
-  ${"" /* display: grid; */}
-  ${"" /* grid-template: */}
-	${"" /* 'name install'; */}
+  width: fit-content;
+  display: grid;
+  padding: 0.5em;
+  gap: 0.5em;
+  justify-content: space-around;
+  grid-template:
+    "vote img name install"
+    "vote img description install";
+  > img {
+    grid-area: img;
+    height: 40px;
+  }
+  p {
+    padding: 0;
+    margin: 0;
+    text-align: left;
+  }
+  > p:first-of-type {
+    grid-area: name;
+  }
+  > p:last-of-type {
+    grid-area: description;
+    color: ${transparentize(0.25, "#24292E")};
+  }
+  * {
+    ${"" /* border: 1px solid red; */}
+  }
 `;
 
 const SnapItem = () => {
@@ -19,8 +47,10 @@ const SnapItem = () => {
   // const { name, img, description } = snapItem
   return (
     <Wrap>
-      {/* <img src={img} alt=""/> */}
-      <p>Name</p>
+      <Vote />
+      <img src={fil} alt="Filecoin Logo" />
+      <p>Filecoin Snap</p>
+      <p>Filecoin Snap Description</p>
       <InstallButton />
     </Wrap>
   );
