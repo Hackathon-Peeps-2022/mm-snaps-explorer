1aac2db2c3dd31f0ad77e821af1d25eb949493f5
Fermin Carranza <fcarr004@fiu.edu>
2022-05-13 17:38:36 -0400

    delte unused

--- src/ethereum/web3.js
+++
@@ -1,9 +0,0 @@
-import { ethers } from 'ethers';
-import MetamaskSnapsExplorer from '../contracts/MetamaskSnapsExplorer.json';
-
-const contractAddress = process.env.METAMASK_SNAPS_EXPLORER_CONTRACT;
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-const contract = new ethers.Contract(contractAddress, MetamaskSnapsExplorer.json, provider.getSigner());
-
-export { provider, contract };
